ae7f33b70abd93b029590f4bda7fac06db6698ac
MasadRayan <masadrayan2002@gmail.com>
2025-11-15 01:57:05 +0600

    router added in index js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/service", require("./service.routes"));
+router.use("/user", require("./user.routes"))
 // router.use("/user", require("./user.routes")); // future collections
 
 module.exports = router;
