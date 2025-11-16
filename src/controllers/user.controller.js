397360ec22fc25a630ac318a7f925ec958ab99fd
MasadRayan <masadrayan2002@gmail.com>
2025-11-16 21:14:16 +0600

    logic added in controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,12 @@
+const usersCollection = require("../models/user.model");
+
+const getAllUsers = async (req, res) => {
+    const result = await usersCollection.find().toArray();
+    res.send(result);
+}
+
+const addUser = async (req, res) => {
+    const user = req.body;
+    const result = await usersCollection.insertOne(user);
+    res.send(result);
+}
