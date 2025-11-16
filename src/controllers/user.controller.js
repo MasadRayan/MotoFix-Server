37a2b8c7cf0e86cb7576292a50e0787e7a784a33
MasadRayan <masadrayan2002@gmail.com>
2025-11-16 23:10:35 +0600

    password bcrypted and stored in the database

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const usersCollection = require("../models/user.model");
+const bcrypt = require("bcryptjs");
 
 const getAllUsers = async (req, res) => {
     const result = await usersCollection.find().toArray();
@@ -7,8 +8,28 @@ const getAllUsers = async (req, res) => {
 
 const addUser = async (req, res) => {
     const user = req.body;
+    const { name, email, password } = user;
+
+    // validation for all fields
+    if (!name || !email || !password) {
+        return res.send({ message: "All fields are required" });
+    }
+
+    // validation of smae user
+    const oldUser = await usersCollection.findOne({ email: user.email });
+    if (oldUser) {
+        return res.send({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user.password = hashedPassword;
+
     const result = await usersCollection.insertOne(user);
     res.send(result);
+
+
+
 }
 
-module.exports = {getAllUsers, addUser}
+module.exports = { getAllUsers, addUser }
