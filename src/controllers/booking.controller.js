9bce1202c2ca55e08111a0d908a615564340f922
MasadRayan <masadrayan2002@gmail.com>
2025-11-18 01:35:19 +0600

    booking deleting controller added

--- src/controllers/booking.controller.js
+++ src/controllers/booking.controller.js
@@ -1,5 +1,5 @@
 const bookingsCollection = require("../models/booking.model");
-
+const {ObjectId} = require("mongodb");
 
 exports.getAllBooking = async (req, res) => {
     const result = await bookingsCollection.find().toArray();
@@ -15,5 +15,13 @@ exports.getUserSpecificBooking = async (req, res) => {
 exports.createBooking = async (req, res) => {
     const booking = req.body;
     const result = await bookingsCollection.insertOne(booking);
+    res.send(result);
+}
+
+
+exports.deletSingleBooking = async (req, res) => {
+    const {id} = req.params;
+    const query = {_id : new ObjectId(id)};
+    const result = await bookingsCollection.deleteOne(query);
     res.send(result);
 }
