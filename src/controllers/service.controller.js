6a247c48d1abe899d053fa1078323eeddf881a6d
MasadRayan <masadrayan2002@gmail.com>
2025-11-22 22:35:06 +0600

    Get collection for admin

--- src/controllers/service.controller.js
+++ src/controllers/service.controller.js
@@ -1,5 +1,6 @@
 const {ObjectId} = require("mongodb");
 const serviceCollection = require("../models/service.model");
+const usersCollection = require("../models/user.model");
 
 exports.getServices = async (req, res) => {
     const result = await serviceCollection.find().toArray();
@@ -12,6 +13,25 @@ exports.getSingleService = async (req, res) => {
     const result = await serviceCollection.findOne(query);
     res.send(result)
 };
+
+exports.getAllServiceForAdmin = async (req, res) => {
+    const {email} = req.params;
+    const query = {email:email};
+    if (!email) {
+        return res.status(400).send({ message: "Email is required" });
+    }
+    const userExists = await usersCollection.findOne(query);
+    if (!userExists) {
+        return res.status(404).send({ message: "User not found" });
+    }
+
+    const usAdmin = userExists.role === "admin";
+    if (!usAdmin) {
+        return res.status(403).send({ message: "Unauthorized" });
+    }
+    const result = await serviceCollection.find().toArray();
+    res.send(result);
+}
 
 exports.createService = async (req, res) => {
     const service = req.body;
