cac50fe188a9e0eb8a922c68a1fe134caacfb188
MasadRayan <masadrayan2002@gmail.com>
2025-11-22 22:58:42 +0600

    Only admin access services added

--- src/routes/service.routes.js
+++ src/routes/service.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getServices, createService, createManyServices, getSingleService } = require("../controllers/service.controller");
+const { getServices, createService, createManyServices, getSingleService, getAllServiceForAdmin } = require("../controllers/service.controller");
 const router = express.Router();
 
 router.get("/", getServices);
+router.get("/admin/:email", getAllServiceForAdmin);
 router.get("/:id", getSingleService);
 router.post("/", createService);
 router.post("/many", createManyServices);
