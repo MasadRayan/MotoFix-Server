c99f98064793382f7011ba45fc6e00e3a3c62c3b
MasadRayan <masadrayan2002@gmail.com>
2025-11-23 00:09:05 +0600

    All booking for admin routes added

--- src/routes/booking.routes.js
+++ src/routes/booking.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get("/", getAllBooking);
 router.get("/user/:email", getUserSpecificBooking);
 router.get("/single/:id", getSingleBooking);
-
+router.get("/admin/:email", getBookingForAdmin);
 router.post("/", createBooking);
 router.delete("/:id", deletSingleBooking);
 router.patch("/bookingUpdate/:id", updateSingleBooking)
