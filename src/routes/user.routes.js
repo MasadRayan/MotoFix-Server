620a892952f6c8498b4203310b89ae3fb496abfb
MasadRayan <masadrayan2002@gmail.com>
2025-11-23 22:52:57 +0600

    user info routes added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");;
 const router = express.Router();
 const {getAllUsers, addUser, loginUser, postSocialLogin, getUserRoale, makeAdmin} = require("../controllers/user.controller");
+const { getUserOverview } = require("../controllers/userInfo.controller");
 
 router.get("/allUser/:email", getAllUsers);
 router.get("/role/:email", getUserRoale);
+router.get("/overview/:email", getUserOverview);
 router.post("/", addUser);
 router.post("/login", loginUser);
 router.post("/social-login", postSocialLogin);
